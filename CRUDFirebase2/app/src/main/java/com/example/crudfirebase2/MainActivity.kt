119433b918305bba5b6c608a9341e4c75795069a
virgo21510023 <virgo_21510023@stimata.ac.id>
2023-03-12 20:18:17 +0700

    Sudah berhasil sampai langkah C
    data sudah bisa tersimpan ke realtime database :smile:

--- CRUDFirebase2/app/src/main/java/com/example/crudfirebase2/MainActivity.kt
+++ CRUDFirebase2/app/src/main/java/com/example/crudfirebase2/MainActivity.kt
@@ -10,6 +10,8 @@ import com.firebase.ui.auth.AuthUI
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -37,6 +39,44 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         when (v.getId()) {
             R.id.save -> {
 // Statement program untuk simpan data
+                // Statement program untuk simpan data
+//Mendapatkan UserID dari pengguna yang Terautentikasi
+                val getUserID = auth!!.currentUser!!.uid
+//Mendapatkan Instance dari Database
+                val database = FirebaseDatabase.getInstance()
+
+//Menyimpan Data yang diinputkan User kedalam Variable
+                val getNIM: String = nim.getText().toString()
+                val getNama: String = nama.getText().toString()
+                val getJurusan: String = jurusan.getText().toString()
+// Mendapatkan Referensi dari Database
+                val getReference: DatabaseReference
+                getReference = database.reference
+// Mengecek apakah ada data yang kosong
+                if (isEmpty(getNIM) || isEmpty(getNama) || isEmpty(getJurusan)) {
+//Jika Ada, maka akan menampilkan pesan singkan seperti berikut ini
+                    Toast.makeText(
+                        this@MainActivity, "Data tidak boleh ada yang kosong ", Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+/* Jika Tidak, maka data dapat diproses dan
+meyimpannya pada Database Menyimpan data
+referensi pada Database berdasarkan User ID dari masing-masing Akun
+*/
+                    getReference.child("Admin").child(getUserID).child("Mahasiswa").push()
+                        .setValue(data_mahasiswa(getNIM, getNama, getJurusan))
+                        .addOnCompleteListener(this) { //Peristiwa ini terjadi saat user berhasil menyimpan datanya kedalam Database
+                            nim.setText("")
+                            nama.setText("")
+                            jurusan.setText("")
+
+                            Toast.makeText(
+                                this@MainActivity, "Data Tersimpan",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                }
+
             }
             R.id.logout ->
 // Statement program untuk logout/keluar
