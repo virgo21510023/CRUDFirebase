d3539b5d2e2f401350dd91df66ddd188ce948f87
virgo21510023 <virgo_21510023@stimata.ac.id>
2023-03-23 18:39:10 +0700

    Sampai Langkah D
    masih ada error coding pada file RecyclerViewAdapter

---
+++ CRUDFirebase2/app/src/main/java/com/example/crudfirebase2/MyListData.kt
@@ -0,0 +1,11 @@
+package com.example.crudfirebase2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyListData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_list_data)
+    }
+}
