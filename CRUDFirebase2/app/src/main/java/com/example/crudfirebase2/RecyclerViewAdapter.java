4dc4a16bf33c05beb331a7b3cf71445a970407aa
virgo21510023 <virgo_21510023@stimata.ac.id>
2023-04-10 19:49:49 +0700

    Langkah E. Fungsi Update
    masih terkendala error pada coding bagian method onLongClickListener(),

--- CRUDFirebase2/app/src/main/java/com/example/crudfirebase2/RecyclerViewAdapter.java
+++ CRUDFirebase2/app/src/main/java/com/example/crudfirebase2/RecyclerViewAdapter.java
@@ -1,8 +1,6 @@
 package com.example.crudfirebase2;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,7 +31,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return new ViewHolder(V);
     }
 
-    public boolean onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         final String NIM = listMahasiswa.get(position).getNim();
         final String Nama = listMahasiswa.get(position).getNama();
         final String Jurusan = listMahasiswa.get(position).getJurusan();
@@ -41,66 +40,27 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         holder.NIM.setText("NIM : " + NIM);
         holder.Nama.setText("Nama : " + Nama);
         holder.Jurusan.setText("Jurusan : " + Jurusan);
-
-        holder.ListItem.setOnLongClickListener(object:View.OnLongClickListener {
-            override fun onLongClick(v:View ?):Boolean {
-                holder.ListItem.setOnLongClickListener {
-                    view ->
-                            val action = arrayOf("Update", "Delete")
-                    val alert:AlertDialog.Builder = AlertDialog.Builder(view.context)
-                    alert.setItems(action, DialogInterface.OnClickListener {
-                        dialog, i ->
-                                when(i);
-                        {
-                            0 ->{
-                            val bundle = Bundle();
-
-                            bundle.putString("dataNIM", listMahasiswa.get(position).nim)
-                            bundle.putString("dataNama", listMahasiswa.get(position).nama)
-                            bundle.putString("dataJurusan", listMahasiswa.get(position).jurusan)
-                            bundle.putString("getPrimaryKey", listMahasiswa.get(position).key)
-                            val intent = Intent(view.context, UpdateData:: class.java)
-                            intent.putExtras(bundle)
-                            context.startActivity(intent)
-                        }
-                            1 ->{
-                        }
-                        }
-                    })
-                    alert.create()
-                    alert.show()
-                    true
-                }
-                return true;
-            }
-        }
     }
 
-    private val Bundle() {
-    }
-}
-;
-                    }
-
-@Override
-public int getItemCount(){
+    @Override
+    public int getItemCount() {
         return listMahasiswa.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView NIM, Nama, Jurusan;
+        private LinearLayout ListItem;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            NIM = itemView.findViewById(R.id.nimx);
+            Nama = itemView.findViewById(R.id.namax);
+            Jurusan = itemView.findViewById(R.id.jurusanx);
+            ListItem = itemView.findViewById(R.id.list_item);
         }
+    }
 
-public class ViewHolder extends RecyclerView.ViewHolder {
-    public View ListItem;
-    private TextView NIM, Nama, Jurusan;
+    public interface datalistener {
 
-    public ViewHolder(@NonNull View itemView) {
-        super(itemView);
-        NIM = itemView.findViewById(R.id.nimx);
-        Nama = itemView.findViewById(R.id.namax);
-        Jurusan = itemView.findViewById(R.id.jurusanx);
-        ListItem = itemView.findViewById(R.id.list_item);
     }
 }
-
-public interface datalistener {
-
-}
-                }
